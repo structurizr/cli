0a7b14d7322edd7daa5766313fc497b59ff3dc1e
Simon Brown <simon@structurizr.com>
2020-06-19 17:30:54 +0100

    Updated libs, and automatic deployment diagrams addition when no views are specified.

--- src/main/java/com/structurizr/cli/PushCommand.java
+++ src/main/java/com/structurizr/cli/PushCommand.java
@@ -7,12 +7,15 @@ import com.structurizr.documentation.AutomaticDocumentationTemplate;
 import com.structurizr.dsl.StructurizrDslParser;
 import com.structurizr.encryption.AesEncryptionStrategy;
 import com.structurizr.model.Container;
+import com.structurizr.model.DeploymentNode;
 import com.structurizr.model.SoftwareSystem;
 import com.structurizr.util.StringUtils;
 import com.structurizr.view.*;
 import org.apache.commons.cli.*;
 
 import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
 
 class PushCommand extends AbstractCommand {
 
@@ -176,26 +179,43 @@ class PushCommand extends AbstractCommand {
         SystemLandscapeView systemLandscapeView = workspace.getViews().createSystemLandscapeView("SystemLandscape", "A system landscape view for " + workspace.getName());
         systemLandscapeView.addAllElements();
         systemLandscapeView.enableAutomaticLayout(AutomaticLayout.RankDirection.TopBottom, 300, 300);
+        systemLandscapeView.setEnterpriseBoundaryVisible(true);
 
         // and a system context view plus container view for each software system
         for (SoftwareSystem softwareSystem : workspace.getModel().getSoftwareSystems()) {
             SystemContextView systemContextView = workspace.getViews().createSystemContextView(softwareSystem, "SystemContext-" + encode(softwareSystem.getName()), "A system context view for " + softwareSystem.getName() + ".");
             systemContextView.addNearestNeighbours(softwareSystem);
             systemContextView.enableAutomaticLayout(AutomaticLayout.RankDirection.TopBottom, 300, 300);
+            systemContextView.setEnterpriseBoundaryVisible(true);
 
             if (softwareSystem.getContainers().size() > 0) {
                 ContainerView containerView = workspace.getViews().createContainerView(softwareSystem, "Containers-" + encode(softwareSystem.getName()), "A container view for " + softwareSystem.getName() + ".");
                 softwareSystem.getContainers().forEach(containerView::addNearestNeighbours);
                 containerView.enableAutomaticLayout(AutomaticLayout.RankDirection.TopBottom, 300, 300);
+                containerView.setExternalSoftwareSystemBoundariesVisible(true);
 
                 for (Container container : softwareSystem.getContainers()) {
                     if (container.getComponents().size() > 0) {
                         ComponentView componentView = workspace.getViews().createComponentView(container, "Components-" + encode(container.getName()), "A component view for " + container.getName() + ".");
                         container.getComponents().forEach(componentView::addNearestNeighbours);
                         componentView.enableAutomaticLayout(AutomaticLayout.RankDirection.TopBottom, 300, 300);
+                        componentView.setExternalSoftwareSystemBoundariesVisible(true);
                     }
                 }
             }
+        }
+
+        // and deployment views for each environment
+        Set<String> deploymentEnvironments = new HashSet<>();
+        for (DeploymentNode deploymentNode : workspace.getModel().getDeploymentNodes()) {
+            deploymentEnvironments.add(deploymentNode.getEnvironment());
+        }
+
+        for (String deploymentEnvironment : deploymentEnvironments) {
+            DeploymentView deploymentView = workspace.getViews().createDeploymentView("Deployment-" + encode(deploymentEnvironment), "A deployment view for " + deploymentEnvironment);
+            deploymentView.setEnvironment(deploymentEnvironment);
+            deploymentView.addAllDeploymentNodes();
+            deploymentView.enableAutomaticLayout(AutomaticLayout.RankDirection.TopBottom, 300, 300);
         }
     }
 
