7c5361aa6bfb3cd9e2d8d3e47c96ca4cb545c8d0
Simon Brown <simon@structurizr.com>
2020-06-20 13:59:47 +0100

    Simplify how view keys are autogenerated.

--- src/main/java/com/structurizr/cli/AbstractCommand.java
+++ src/main/java/com/structurizr/cli/AbstractCommand.java
@@ -7,6 +7,7 @@ import com.structurizr.model.SoftwareSystem;
 import com.structurizr.model.Tags;
 import com.structurizr.view.*;
 
+import java.text.DecimalFormat;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -28,27 +29,27 @@ abstract class AbstractCommand {
             System.out.println(" - no views defined; creating default views");
 
             // create a single System Landscape diagram containing all people and software systems
-            SystemLandscapeView systemLandscapeView = workspace.getViews().createSystemLandscapeView("SystemLandscape", "A system landscape view for " + workspace.getName());
+            SystemLandscapeView systemLandscapeView = workspace.getViews().createSystemLandscapeView(generateViewKey(workspace, "SystemLandscape"), "A system landscape view for " + workspace.getName());
             systemLandscapeView.addAllElements();
             systemLandscapeView.enableAutomaticLayout(AutomaticLayout.RankDirection.TopBottom, 300, 300);
             systemLandscapeView.setEnterpriseBoundaryVisible(true);
 
             // and a system context view plus container view for each software system
             for (SoftwareSystem softwareSystem : workspace.getModel().getSoftwareSystems()) {
-                SystemContextView systemContextView = workspace.getViews().createSystemContextView(softwareSystem, "SystemContext-" + encode(softwareSystem.getName()), "A system context view for " + softwareSystem.getName() + ".");
+                SystemContextView systemContextView = workspace.getViews().createSystemContextView(softwareSystem, generateViewKey(workspace, "SystemContext"), "A system context view for " + softwareSystem.getName() + ".");
                 systemContextView.addNearestNeighbours(softwareSystem);
                 systemContextView.enableAutomaticLayout(AutomaticLayout.RankDirection.TopBottom, 300, 300);
                 systemContextView.setEnterpriseBoundaryVisible(true);
 
                 if (softwareSystem.getContainers().size() > 0) {
-                    ContainerView containerView = workspace.getViews().createContainerView(softwareSystem, "Containers-" + encode(softwareSystem.getName()), "A container view for " + softwareSystem.getName() + ".");
+                    ContainerView containerView = workspace.getViews().createContainerView(softwareSystem, generateViewKey(workspace, "Container"), "A container view for " + softwareSystem.getName() + ".");
                     softwareSystem.getContainers().forEach(containerView::addNearestNeighbours);
                     containerView.enableAutomaticLayout(AutomaticLayout.RankDirection.TopBottom, 300, 300);
                     containerView.setExternalSoftwareSystemBoundariesVisible(true);
 
                     for (Container container : softwareSystem.getContainers()) {
                         if (container.getComponents().size() > 0) {
-                            ComponentView componentView = workspace.getViews().createComponentView(container, "Components-" + encode(container.getName()), "A component view for " + container.getName() + ".");
+                            ComponentView componentView = workspace.getViews().createComponentView(container, generateViewKey(workspace, "Component"), "A component view for " + container.getName() + ".");
                             container.getComponents().forEach(componentView::addNearestNeighbours);
                             componentView.enableAutomaticLayout(AutomaticLayout.RankDirection.TopBottom, 300, 300);
                             componentView.setExternalSoftwareSystemBoundariesVisible(true);
@@ -64,7 +65,7 @@ abstract class AbstractCommand {
             }
 
             for (String deploymentEnvironment : deploymentEnvironments) {
-                DeploymentView deploymentView = workspace.getViews().createDeploymentView("Deployment-" + encode(deploymentEnvironment), "A deployment view for " + deploymentEnvironment);
+                DeploymentView deploymentView = workspace.getViews().createDeploymentView(generateViewKey(workspace, "Deployment"), "A deployment view for " + deploymentEnvironment);
                 deploymentView.setEnvironment(deploymentEnvironment);
                 deploymentView.addAllDeploymentNodes();
                 deploymentView.enableAutomaticLayout(AutomaticLayout.RankDirection.TopBottom, 300, 300);
@@ -82,8 +83,9 @@ abstract class AbstractCommand {
         }
     }
 
-    private String encode(String name) {
-        return name.replaceAll("\\s", "");
+    private String generateViewKey(Workspace workspace, String type) {
+        DecimalFormat format = new DecimalFormat("000");
+        return format.format(workspace.getViews().getViews().size() + 1) + "-" + type;
     }
 
 }
