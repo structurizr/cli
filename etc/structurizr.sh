1866a453b3d39af3d19a3f7435fae28b9295d57b
Simon Brown <simon@structurizr.com>
2020-10-12 06:23:31 +0100

    Fixes #9.

--- etc/structurizr.sh
+++ etc/structurizr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-java -jar $SCRIPT_DIR/structurizr-cli-1.4.4.jar "$@"
+java -jar $SCRIPT_DIR/structurizr-cli-1.4.5.jar "$@"
