3db48ae65c8b81d109dda0cb042b5000d1c36499
Paul Colby <git@colby.id.au>
2022-07-20 14:58:47 +1000

    Prevent accidental globbing and word splitting
    See https://www.shellcheck.net/wiki/SC2086

--- etc/structurizr.sh
+++ etc/structurizr.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-java -cp $SCRIPT_DIR:$SCRIPT_DIR/lib/* com.structurizr.cli.StructurizrCliApplication "$@"
+java -cp "$SCRIPT_DIR:$SCRIPT_DIR"/lib/* com.structurizr.cli.StructurizrCliApplication "$@"
