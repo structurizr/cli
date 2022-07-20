b8497dcf55f1b36ddb2d5b7d5bb239defbf5c2af
Paul Colby <git@colby.id.au>
2022-07-20 15:06:10 +1000

    Apply some Bash best practices
    Specifically, use `/usr/bin/env` in shebang for [portability], and
    set Bash "strict-mode" options for safer error handling.
    
    [portability]: https://en.wikipedia.org/w/index.php?title=Shebang_(Unix)#Portability

--- etc/structurizr.sh
+++ etc/structurizr.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -o errexit -o noclobber -o nounset -o pipefail
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 java -cp "$SCRIPT_DIR:$SCRIPT_DIR"/lib/* com.structurizr.cli.StructurizrCliApplication "$@"
