8213e2e9bbeaba01c1f403c0c744d125ddeebb55
Simon Brown <1009874+simonbrowndotje@users.noreply.github.com>
2025-02-21 23:25:20 +0000

    Adds an initial implementation of a static HTML export.

--- src/main/java/com/structurizr/cli/export/ExportCommand.java
+++ src/main/java/com/structurizr/cli/export/ExportCommand.java
@@ -1,8 +1,9 @@
 package com.structurizr.cli.export;
 
 import com.structurizr.Workspace;
+import com.structurizr.autolayout.graphviz.GraphvizAutomaticLayout;
 import com.structurizr.cli.AbstractCommand;
-import com.structurizr.dsl.StructurizrDslParser;
+import com.structurizr.documentation.Documentable;
 import com.structurizr.export.*;
 import com.structurizr.export.dot.DOTExporter;
 import com.structurizr.export.ilograph.IlographExporter;
@@ -17,15 +18,15 @@ import org.apache.commons.cli.*;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.net.URL;
-import java.net.URLClassLoader;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.util.Base64;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 public class ExportCommand extends AbstractCommand {
 
@@ -41,6 +42,7 @@ public class ExportCommand extends AbstractCommand {
     private static final String DOT_FORMAT = "dot";
     private static final String ILOGRAPH_FORMAT = "ilograph";
     private static final String D2_FORMAT = "d2";
+    private static final String STATIC_FORMAT = "static";
     private static final String CUSTOM_FORMAT = "fqcn";
 
     private static final Map<String,Exporter> EXPORTERS = new HashMap<>();
@@ -112,12 +114,6 @@ public class ExportCommand extends AbstractCommand {
 
         workspaceId = workspace.getId();
 
-        if (!JSON_FORMAT.equalsIgnoreCase(format)) {
-            // only inline the theme amd create default views if the user wants a diagram export
-            ThemeUtils.loadThemes(workspace);
-            addDefaultViewsAndStyles(workspace);
-        }
-
         if (outputPath == null) {
             outputPath = new File(workspacePath.getCanonicalPath()).getParent();
         }
@@ -125,57 +121,121 @@ public class ExportCommand extends AbstractCommand {
         File outputDir = new File(outputPath);
         outputDir.mkdirs();
 
-       Exporter exporter = findExporter(format, workspacePath);
-        if (exporter == null) {
-            log.info(" - unknown export format: " + format);
+        if (STATIC_FORMAT.equals(format)) {
+            log.info(" - writing static site to " + outputDir.getAbsolutePath());
+            unzip(getClass().getResourceAsStream("/static.zip"), outputPath);
+
+            // add default views if no views exist
+            addDefaultViewsAndStyles(workspace);
+
+            // clear all documentation - this isn't supported by the static site
+            workspace.getDocumentation().clear();
+            workspace.getModel().getElements().stream().filter(e -> e instanceof Documentable).map(e -> (Documentable)e).forEach(e -> e.getDocumentation().clear());
+
+            // apply Graphviz locally - the static site can't do this
+            File tmpdir = Files.createTempDirectory("graphviz").toFile();
+            tmpdir.deleteOnExit();
+            log.debug("Graphviz working directory is " + tmpdir.getAbsolutePath());
+            new GraphvizAutomaticLayout(tmpdir).apply(workspace);
+
+            String json = WorkspaceUtils.toJson(workspace, false);
+            String base64 = Base64.getEncoder().encodeToString(json.getBytes(StandardCharsets.UTF_8));
+
+            writeToFile(
+                    new File(outputDir, "workspace.js"),
+                    String.format("const jsonAsString = '%s';", base64)
+            );
+
         } else {
-            log.info(" - exporting with " + exporter.getClass().getSimpleName());
+            if (!JSON_FORMAT.equalsIgnoreCase(format)) {
+                // only inline the theme amd create default views if the user wants a diagram export
+                ThemeUtils.loadThemes(workspace);
+                addDefaultViewsAndStyles(workspace);
+            }
 
-            if (exporter instanceof DiagramExporter) {
-                DiagramExporter diagramExporter = (DiagramExporter) exporter;
+            Exporter exporter = findExporter(format, workspacePath);
+            if (exporter == null) {
+                log.info(" - unknown export format: " + format);
+            } else {
+                log.info(" - exporting with " + exporter.getClass().getSimpleName());
 
-                if (workspace.getViews().isEmpty()) {
-                    log.info(" - the workspace contains no views");
-                } else {
-                    Collection<Diagram> diagrams = diagramExporter.export(workspace);
+                if (exporter instanceof DiagramExporter) {
+                    DiagramExporter diagramExporter = (DiagramExporter) exporter;
 
-                    for (Diagram diagram : diagrams) {
-                        File file = new File(outputPath, String.format("%s-%s.%s", prefix(workspaceId), diagram.getKey(), diagram.getFileExtension()));
-                        writeToFile(file, diagram.getDefinition());
+                    if (workspace.getViews().isEmpty()) {
+                        log.info(" - the workspace contains no views");
+                    } else {
+                        Collection<Diagram> diagrams = diagramExporter.export(workspace);
 
-                        if (diagram.getLegend() != null) {
-                            file = new File(outputPath, String.format("%s-%s-key.%s", prefix(workspaceId), diagram.getKey(), diagram.getFileExtension()));
-                            writeToFile(file, diagram.getLegend().getDefinition());
-                        }
+                        for (Diagram diagram : diagrams) {
+                            File file = new File(outputPath, String.format("%s-%s.%s", prefix(workspaceId), diagram.getKey(), diagram.getFileExtension()));
+                            writeToFile(file, diagram.getDefinition());
 
-                        if (!diagram.getFrames().isEmpty()) {
-                            int index = 1;
-                            for (Diagram frame : diagram.getFrames()) {
-                                file = new File(outputPath, String.format("%s-%s-%s.%s", prefix(workspaceId), diagram.getKey(), index, diagram.getFileExtension()));
-                                writeToFile(file, frame.getDefinition());
-                                index++;
+                            if (diagram.getLegend() != null) {
+                                file = new File(outputPath, String.format("%s-%s-key.%s", prefix(workspaceId), diagram.getKey(), diagram.getFileExtension()));
+                                writeToFile(file, diagram.getLegend().getDefinition());
+                            }
+
+                            if (!diagram.getFrames().isEmpty()) {
+                                int index = 1;
+                                for (Diagram frame : diagram.getFrames()) {
+                                    file = new File(outputPath, String.format("%s-%s-%s.%s", prefix(workspaceId), diagram.getKey(), index, diagram.getFileExtension()));
+                                    writeToFile(file, frame.getDefinition());
+                                    index++;
+                                }
                             }
                         }
                     }
+                } else if (exporter instanceof WorkspaceExporter) {
+                    WorkspaceExporter workspaceExporter = (WorkspaceExporter) exporter;
+                    WorkspaceExport export = workspaceExporter.export(workspace);
+
+                    String filename;
+
+                    if (THEME_FORMAT.equalsIgnoreCase(format)) {
+                        filename = workspacePath.getName().substring(0, workspacePath.getName().lastIndexOf('.')) + "-theme";
+                    } else {
+                        filename = workspacePath.getName().substring(0, workspacePath.getName().lastIndexOf('.'));
+                    }
+
+                    File file = new File(outputPath, String.format("%s.%s", filename, export.getFileExtension()));
+                    writeToFile(file, export.getDefinition());
                 }
-            } else if (exporter instanceof WorkspaceExporter) {
-                WorkspaceExporter workspaceExporter = (WorkspaceExporter) exporter;
-                WorkspaceExport export = workspaceExporter.export(workspace);
-
-                String filename;
-
-                if (THEME_FORMAT.equalsIgnoreCase(format)) {
-                    filename = workspacePath.getName().substring(0, workspacePath.getName().lastIndexOf('.')) + "-theme";
-                } else {
-                    filename = workspacePath.getName().substring(0, workspacePath.getName().lastIndexOf('.'));
-                }
-
-                File file = new File(outputPath, String.format("%s.%s", filename, export.getFileExtension()));
-                writeToFile(file, export.getDefinition());
             }
         }
 
         log.info(" - finished");
+    }
+
+    private void unzip(InputStream inputStream, String destinationDirectory) {
+        byte[] buffer = new byte[1024];
+
+        try {
+            ZipInputStream zis = new ZipInputStream(inputStream);
+            ZipEntry ze = zis.getNextEntry();
+            while (ze != null) {
+                if (!ze.isDirectory()) {
+                    String fileName = ze.getName();
+                    File destinationFile = new File(destinationDirectory + File.separator + fileName);
+
+                    new File(destinationFile.getParent()).mkdirs();
+                    FileOutputStream fos = new FileOutputStream(destinationFile);
+                    int len;
+                    while ((len = zis.read(buffer)) > 0) {
+                        fos.write(buffer, 0, len);
+                    }
+                    fos.close();
+                }
+                zis.closeEntry();
+                ze = zis.getNextEntry();
+            }
+
+            zis.closeEntry();
+            zis.close();
+            inputStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private Exporter findExporter(String format, File workspacePath) {
---
+++ ui.sh
@@ -0,0 +1,49 @@
+#!/bin/zsh
+
+# - this script merges the contents of the structurizr/ui repository into this directory,
+# - this has only been tested on MacOS
+
+export STRUCTURIZR_BUILD_NUMBER=$1
+export STRUCTURIZR_UI_DIR=../structurizr-ui
+export STRUCTURIZR_CLI_DIR=.
+
+rm -rf $STRUCTURIZR_CLI_DIR/src/main/resources/static
+mkdir -p $STRUCTURIZR_CLI_DIR/src/main/resources/static
+
+# JavaScript
+mkdir -p $STRUCTURIZR_CLI_DIR/src/main/resources/static/js
+cp -a $STRUCTURIZR_UI_DIR/src/js/jquery-3.6.3.min.js $STRUCTURIZR_CLI_DIR/src/main/resources/static/js
+cp -a $STRUCTURIZR_UI_DIR/src/js/bootstrap-3.3.7.min.js $STRUCTURIZR_CLI_DIR/src/main/resources/static/js
+cp -a $STRUCTURIZR_UI_DIR/src/js/lodash-4.17.21.js $STRUCTURIZR_CLI_DIR/src/main/resources/static/js
+cp -a $STRUCTURIZR_UI_DIR/src/js/backbone-1.4.1.js $STRUCTURIZR_CLI_DIR/src/main/resources/static/js
+cp -a $STRUCTURIZR_UI_DIR/src/js/joint-3.6.5.js $STRUCTURIZR_CLI_DIR/src/main/resources/static/js
+cp -a $STRUCTURIZR_UI_DIR/src/js/structurizr.js $STRUCTURIZR_CLI_DIR/src/main/resources/static/js
+cp -a $STRUCTURIZR_UI_DIR/src/js/structurizr-util.js $STRUCTURIZR_CLI_DIR/src/main/resources/static/js
+cp -a $STRUCTURIZR_UI_DIR/src/js/structurizr-ui.js $STRUCTURIZR_CLI_DIR/src/main/resources/static/js
+cp -a $STRUCTURIZR_UI_DIR/src/js/structurizr-workspace.js $STRUCTURIZR_CLI_DIR/src/main/resources/static/js
+cp -a $STRUCTURIZR_UI_DIR/src/js/structurizr-diagram.js $STRUCTURIZR_CLI_DIR/src/main/resources/static/js
+cp -a $STRUCTURIZR_UI_DIR/src/js/structurizr-quick-navigation.js $STRUCTURIZR_CLI_DIR/src/main/resources/static/js
+cp -a $STRUCTURIZR_UI_DIR/src/js/structurizr-navigation.js $STRUCTURIZR_CLI_DIR/src/main/resources/static/js
+cp -a $STRUCTURIZR_UI_DIR/src/js/structurizr-tooltip.js $STRUCTURIZR_CLI_DIR/src/main/resources/static/js
+
+# CSS
+mkdir -p $STRUCTURIZR_CLI_DIR/src/main/resources/static/css
+cp -a $STRUCTURIZR_UI_DIR/src/css/bootstrap-3.3.7.min.css $STRUCTURIZR_CLI_DIR/src/main/resources/static/css
+cp -a $STRUCTURIZR_UI_DIR/src/css/joint-3.6.5.css $STRUCTURIZR_CLI_DIR/src/main/resources/static/css
+cp -a $STRUCTURIZR_UI_DIR/src/css/structurizr-diagram.css $STRUCTURIZR_CLI_DIR/src/main/resources/static/css
+cp -a $STRUCTURIZR_UI_DIR/src/css/structurizr-static.css $STRUCTURIZR_CLI_DIR/src/main/resources/static/css
+cp -a $STRUCTURIZR_UI_DIR/src/css/structurizr-static-dark.css $STRUCTURIZR_CLI_DIR/src/main/resources/static/css
+
+# Bootstrap icons
+mkdir -p $STRUCTURIZR_CLI_DIR/src/main/resources/static/bootstrap-icons
+cp -a $STRUCTURIZR_UI_DIR/src/bootstrap-icons/* $STRUCTURIZR_CLI_DIR/src/main/resources/static/bootstrap-icons
+
+# HTML
+cp -a $STRUCTURIZR_UI_DIR/src/static.html $STRUCTURIZR_CLI_DIR/src/main/resources/static/index.html
+
+cd src/main/resources/static
+rm ../static.zip
+zip -r ../static.zip .
+cd ../../../..
+rm -rf $STRUCTURIZR_CLI_DIR/src/main/resources/static
+
