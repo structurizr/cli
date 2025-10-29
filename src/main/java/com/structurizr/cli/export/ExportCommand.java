8aaa3a0f639a0f856809c155046473cc06900c19
Simon Brown <simon@structurizr.com>
2025-10-29 10:52:07 +0100

    Removes D2 from the help command.

--- src/main/java/com/structurizr/cli/export/ExportCommand.java
+++ src/main/java/com/structurizr/cli/export/ExportCommand.java
@@ -44,7 +44,7 @@ public class ExportCommand extends AbstractCommand {
     private static final String MERMAID_FORMAT = "mermaid";
     private static final String DOT_FORMAT = "dot";
     private static final String ILOGRAPH_FORMAT = "ilograph";
-    private static final String D2_FORMAT = "d2";
+//    private static final String D2_FORMAT = "d2";
     private static final String STATIC_FORMAT = "static";
     private static final String CUSTOM_FORMAT = "fqcn";
 
@@ -84,7 +84,7 @@ public class ExportCommand extends AbstractCommand {
         option.setRequired(true);
         options.addOption(option);
 
-        option = new Option("f", "format", true, String.format("Export format: %s[/%s|%s]|%s|%s|%s|%s|%s|%s|%s|%s|%s", PLANTUML_FORMAT, PLANTUML_STRUCTURIZR_SUBFORMAT, PLANTUML_C4PLANTUML_SUBFORMAT, WEBSEQUENCEDIAGRAMS_FORMAT, MERMAID_FORMAT, DOT_FORMAT, ILOGRAPH_FORMAT, D2_FORMAT, JSON_FORMAT, THEME_FORMAT, STATIC_FORMAT, CUSTOM_FORMAT));
+        option = new Option("f", "format", true, String.format("Export format: %s[/%s|%s]|%s|%s|%s|%s|%s|%s|%s|%s", PLANTUML_FORMAT, PLANTUML_STRUCTURIZR_SUBFORMAT, PLANTUML_C4PLANTUML_SUBFORMAT, WEBSEQUENCEDIAGRAMS_FORMAT, MERMAID_FORMAT, DOT_FORMAT, ILOGRAPH_FORMAT, JSON_FORMAT, THEME_FORMAT, STATIC_FORMAT, CUSTOM_FORMAT));
         option.setRequired(true);
         options.addOption(option);
 
